729e5e0eb981cc79cc54a9d61d7270833629fe46
andygout <andygout@hotmail.co.uk>
2017-11-26 10:07:06 +0000

    Apply: getData module logic directly in Redux model action

--- src/redux/actions/model.js
+++ src/redux/actions/model.js
@@ -1,3 +1,45 @@
-import getData from './utils/get-data';
+import * as actions from './utils/actions-list';
+import createAction from './base';
+import { setError } from './error';
+import { irregularPluralNouns } from '../../utils/constants';
 
-export default (model, uuid = null) => (dispatch, getState) => getData(dispatch, getState, model, uuid ? { uuid } : {});
+const request = model => createAction(actions[`REQUEST_${model.toUpperCase()}`]);
+
+const receive = (instance, model) => createAction(actions[`RECEIVE_${model.toUpperCase()}`], instance);
+
+export default (model, uuid = null) => (dispatch, getState) => {
+
+	if (getState().error.exists) dispatch(setError({ exists: false, message: null }));
+
+	const instance = uuid ? true : false;
+
+	const apiCallReqd = instance ?
+		getState()[model].uuid !== uuid :
+		!getState()[model].length;
+
+	if (apiCallReqd) {
+
+		dispatch(request(model));
+
+		let url = 'http://localhost:3000/';
+
+		url += instance ?
+			irregularPluralNouns[model] || model + 's' :
+			model;
+
+		if (instance) url += `/${uuid}`;
+
+		return fetch(url, { 'mode': 'cors' })
+			.then(response => {
+
+				if (response.status !== 200) throw new Error(response.statusText);
+
+				return response.json();
+
+			})
+			.then(instance => dispatch(receive(instance, model)))
+			.catch(({ message }) => dispatch(setError({ exists: true, message })));
+
+	}
+
+}
