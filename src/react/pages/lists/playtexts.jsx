aaa0f047ba58d163c988129ff3ea3058c06ad08a
andygout <andygout@hotmail.co.uk>
2018-08-26 15:09:42 +0100

    Remove: Unused Link import from list pages

--- src/react/pages/lists/playtexts.jsx
+++ src/react/pages/lists/playtexts.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 import ListWrapper from '../../utils/list-wrapper';
 
