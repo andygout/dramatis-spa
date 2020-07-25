b16920b7e8f0400a869026a11cc0a317e780bbd4
andygout <andygout@hotmail.co.uk>
2020-07-25 11:34:27 +0100

    Separate third-party + native dependencies in react-html.jsx

--- src/react/react-html.jsx
+++ src/react/react-html.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import { StaticRouter } from 'react-router-dom';
+
 import App from './App';
 
 export default (request, store) =>
