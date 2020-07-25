12a97b958b3d07e5d8faf9d7dd5f9c5ef4b153f5
andygout <andygout@hotmail.co.uk>
2020-07-25 11:33:54 +0100

    Improve formatting of react-html.jsx

--- src/react/react-html.jsx
+++ src/react/react-html.jsx
@@ -4,14 +4,11 @@ import { Provider } from 'react-redux';
 import { StaticRouter } from 'react-router-dom';
 import App from './App';
 
-export default (request, store) => {
-
-	return renderToString(
+export default (request, store) =>
+	renderToString(
 		<Provider store={store}>
 			<StaticRouter location={request.url} context={{}}>
 				<App />
 			</StaticRouter>
 		</Provider>
 	);
-
-};
