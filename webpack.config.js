93c913135df432112f48f1c2fe95dcfdb3602e71
Andy Gout <andygout@users.noreply.github.com>
2020-08-15 15:05:52 +0100

    Babel + Webpack revisions (#82)
    * Include Node v14 target in .babelrc
    
    * Reorder webpack.config.js dependencies
    
    * Remove unrequired webpack requirement in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
+const path = require('path');
+
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const nodeExternals = require('webpack-node-externals');
-const path = require('path');
-const webpack = require('webpack');
 
 const serverConfig = {
 	mode: 'none', // i.e. not production or development (see: https://webpack.js.org/configuration/mode).
