cddf3bdbc89ddc75e9acea910f5d9b57f843a07d
Andy Gout <andygout@hotmail.co.uk>
2023-07-31 22:55:31 +0100

    Display multiple recipientMaterials + recipientProductions

--- src/react/pages/instances/Production.jsx
+++ src/react/pages/instances/Production.jsx
@@ -4,9 +4,7 @@ import { connect } from 'react-redux';
 
 import { formatDate } from '../../../lib/format-date';
 import {
-	AppendedProductionDates,
 	AppendedRoles,
-	AppendedVenue,
 	CommaSeparatedMaterials,
 	CommaSeparatedProductions,
 	Entities,
@@ -247,32 +245,28 @@ const Production = props => {
 																						}
 
 																						{
-																							nomination.entities.length > 0 && nomination.recipientProduction && (
+																							nomination.entities.length > 0 &&
+																							(nomination.recipientProductions.length > 0 || nomination.coProductions.length > 0) && (
 																								<>{';'}</>
 																							)
 																						}
 
 																						{
-																							nomination.recipientProduction && (
+																							nomination.recipientProductions.length > 0 && (
 																								<>
 																									<>{' (for '}</>
-																									<InstanceLink instance={nomination.recipientProduction} />
-																									{
-																										nomination.recipientProduction.venue && (
-																											<AppendedVenue venue={nomination.recipientProduction.venue} />
-																										)
-																									}
-
-																									{
-																										(nomination.recipientProduction.startDate || nomination.recipientProduction.endDate) && (
-																											<AppendedProductionDates
-																												startDate={nomination.recipientProduction.startDate}
-																												endDate={nomination.recipientProduction.endDate}
-																											/>
-																										)
-																									}
+																									<CommaSeparatedProductions
+																										productions={nomination.recipientProductions}
+																									/>
 																									<>{')'}</>
 																								</>
+																							)
+																						}
+
+																						{
+																							nomination.recipientProductions.length > 0 &&
+																							nomination.coProductions.length > 0 && (
+																								<>{';'}</>
 																							)
 																						}
 
