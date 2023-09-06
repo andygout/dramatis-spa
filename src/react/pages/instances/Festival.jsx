828799abac39bcd15781618c350b0dc6a40e1fe2
Andy Gout <andygout@users.noreply.github.com>
2023-09-06 17:36:22 +0100

    Display production festival (#196)

--- src/react/pages/instances/Festival.jsx
+++ src/react/pages/instances/Festival.jsx
@@ -2,14 +2,29 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { InstanceFacet, ProductionsList } from '../../components';
 import { InstancePageWrapper } from '../../page-wrappers';
 
 const Festival = props => {
 
 	const { festival } = props;
 
+	const { productions } = festival;
+
 	return (
-		<InstancePageWrapper instance={festival} />
+		<InstancePageWrapper instance={festival}>
+
+			{
+				productions?.length > 0 && (
+					<InstanceFacet labelText='Productions'>
+
+						<ProductionsList productions={productions} />
+
+					</InstanceFacet>
+				)
+			}
+
+		</InstancePageWrapper>
 	);
 
 };
