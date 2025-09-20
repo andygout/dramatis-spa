a6442e4ef8965b04a236945ecdc05f5e3c842ef9
Andy Gout <andygout@hotmail.co.uk>
2025-09-20 14:47:24 +0100

    Revise formatDate function

--- test/src/lib/format-date.test.js
+++ test/src/lib/format-date.test.js
@@ -6,9 +6,23 @@ describe('Format Date module', () => {
 
 	context('custom options are not provided', () => {
 
-		it('returns a date formatted as to: DD - abbreviated month - YYYY', () => {
+		context('given month is not September', () => {
 
-			expect(formatDate('2021-04-09')).to.equal('09 Apr 2021');
+			it('returns a date formatted as to: DD - abbreviated month - YYYY', () => {
+
+				expect(formatDate('2021-04-09')).to.equal('09 Apr 2021');
+
+			});
+
+		});
+
+		context('given month is September', () => {
+
+			it('returns a date formatted as to: DD - abbreviated month - YYYY; converts \'Sept\' to \'Sep\'', () => {
+
+				expect(formatDate('2021-09-01')).to.equal('01 Sep 2021');
+
+			});
 
 		});
 
@@ -16,9 +30,23 @@ describe('Format Date module', () => {
 
 	context('custom options are provided', () => {
 
-		it('returns a date formatted by factoring in the custom options', () => {
+		context('given month is not September', () => {
 
-			expect(formatDate('2021-04-09', { weekday: 'long', month: 'long' })).to.equal('Friday, 09 April 2021');
+			it('returns a date formatted by factoring in the custom options; strips commas from literal parts', () => {
+
+				expect(formatDate('2021-04-09', { weekday: 'long', month: 'long' })).to.equal('Friday 09 April 2021');
+
+			});
+
+		});
+
+		context('given month is September', () => {
+
+			it('returns a date formatted by factoring in the custom options; strips commas from literal parts', () => {
+
+				expect(formatDate('2021-09-01', { weekday: 'long', month: 'long' })).to.equal('Wednesday 01 September 2021');
+
+			});
 
 		});
 
