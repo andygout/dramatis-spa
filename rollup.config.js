9867eaab615af689ed91af3dcb0835315d2d331d
Andy Gout <andygout@hotmail.co.uk>
2025-12-08 17:40:13 +0000

    Move favicon.ico to server assets folder

--- rollup.config.js
+++ rollup.config.js
@@ -42,7 +42,7 @@ const serverBundle = {
 		}),
 		copy({
 			targets: [
-				{ src: './src/client/assets/favicon.ico', dest: 'built/assets' }
+				{ src: './src/server/assets/favicon.ico', dest: 'built/assets' }
 			]
 		})
 	]
