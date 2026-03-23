20755c8f81eb70a501eba0b60aae4ff91def4159
Andy Gout <andygout@hotmail.co.uk>
2026-03-23 19:05:03 +0000

    Remove ESLint rules overridden by Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -40,7 +40,6 @@ export default [
 					}
 				}
 			],
-			'comma-dangle': 'error',
 			eqeqeq: 'error',
 			'guard-for-in': 'error',
 			'new-cap': 'error',
@@ -49,19 +48,12 @@ export default [
 			'no-extend-native': 'error',
 			'no-irregular-whitespace': 'error',
 			'no-loop-func': 'error',
-			'no-multi-spaces': 'error',
-			'no-multiple-empty-lines': ['error', { max: 1 }],
 			'no-undef': 'error',
 			'no-underscore-dangle': 'error',
 			'no-unused-vars': 'error',
 			'no-var': 'error',
 			'one-var': ['error', 'never'],
-			quotes: ['error', 'single'],
-			semi: 'error',
-			'space-before-function-paren': 'error',
-			'spaced-comment': 'error',
-			strict: ['error', 'global'],
-			'wrap-iife': 'error'
+			strict: ['error', 'global']
 		}
 	},
 	{
