3954b30ebde15db3f59e38c57cbd50f0fb24f9f7
Andy Gout <andygout@users.noreply.github.com>
2026-05-25 12:10:28 +0100

    Revise header home page link icon implementation (#301)

--- src/react/components/Header.jsx
+++ src/react/components/Header.jsx
@@ -7,11 +7,7 @@ const Header = () => {
 		<header className="header">
 			<div className="header__component">
 				<Link to={'/'} className="header__home-link">
-					<img
-						src="/assets/header-logo-icon.svg"
-						alt="The Dramatis logo: the comedy and tragedy masks framed in a cell"
-						className="header__home-link-icon"
-					/>
+					<span className="header__home-link-icon" aria-hidden="true" />
 					Dramatis
 				</Link>
 			</div>
