1ab469984605818a5b811157c1c841eae217f469
Andy Gout <andygout@users.noreply.github.com>
2026-09-10 16:47:07 +0100

    Display time surTimes + subTimes (#313)

--- src/react/pages/instances/Time.jsx
+++ src/react/pages/instances/Time.jsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router';
 
 import { useGetTimeQuery } from '../../../redux/slices/api.js';
-import { InstanceFacet, MaterialsList } from '../../components/index.js';
+import { InstanceFacet, InstanceLinksList, MaterialsList } from '../../components/index.js';
 import { InstancePageWrapper } from '../../page-wrappers/index.js';
 
 const Time = () => {
@@ -9,10 +9,22 @@ const Time = () => {
 
 	const { data: time = {} } = useGetTimeQuery(uuid);
 
-	const { materials } = time;
+	const { surTimes, subTimes, materials } = time;
 
 	return (
 		<InstancePageWrapper instance={time}>
+			{surTimes?.length > 0 && (
+				<InstanceFacet labelText="Is within">
+					<InstanceLinksList instances={surTimes} />
+				</InstanceFacet>
+			)}
+
+			{subTimes?.length > 0 && (
+				<InstanceFacet labelText="Contains">
+					<InstanceLinksList instances={subTimes} />
+				</InstanceFacet>
+			)}
+
 			{materials?.length > 0 && (
 				<InstanceFacet labelText="Materials as setting">
 					<MaterialsList materials={materials} />
